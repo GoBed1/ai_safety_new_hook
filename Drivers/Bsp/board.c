51f3447d6b80417c45b194afe25035693fa6da94
cjj <curryjamesjie@163.com>
2026-05-30 15:53:22 +0800

    docs: Change the serial port pins of debug

--- Drivers/Bsp/board.c
+++ Drivers/Bsp/board.c
@@ -34,7 +34,7 @@ const osThreadAttr_t HeartbeatTask_attributes = {
 
 void init_app(void)
 {
-  specify_redirect_uart(&huart1);
+  specify_redirect_uart(&huart5);
   printf("\r\n[INFO] [BOARD] specify redirect printf to huart1\r\n");
 
   init_app_hook_task();
